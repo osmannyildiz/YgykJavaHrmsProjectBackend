2debc0c1da95dc1202afa5be882cf453fa83719a
Osman Nuri Yıldız <iamosmannyildiz@gmail.com>
2022-01-01 16:50:26 +0300

    Initial commit

---
+++ src/main/java/osmannyildiz/ygykHrmsProject/YgykHrmsProjectApplication.java
@@ -0,0 +1,13 @@
+package osmannyildiz.ygykHrmsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgykHrmsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YgykHrmsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/osmannyildiz/ygykHrmsProject/YgykHrmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package osmannyildiz.ygykHrmsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YgykHrmsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
